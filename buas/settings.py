7df6b4afe27642fc70d498c1a2fefd63bced0b8f
RotatingCloud <113541115+RotatingCloud@users.noreply.github.com>
2023-09-18 20:18:41 -0700

    .

--- buas/settings.py
+++ buas/settings.py
@@ -161,7 +161,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = config('EMAIL_BACKEND', default='')
 EMAIL_HOST = config('EMAIL_HOST', default='')
-EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
-EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='') 
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
